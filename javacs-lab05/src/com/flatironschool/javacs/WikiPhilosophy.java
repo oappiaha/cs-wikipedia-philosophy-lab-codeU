f9dc4f8d703ee213d36d95d34ec9cc6f257c7c50
Obed Appiah-Agyeman <ooappiahagyeman@gmail.com>
2016-07-02 18:38:18 -0400

    Update WikiPhilosophy.java

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -2,17 +2,22 @@ package com.flatironschool.javacs;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import org.jsoup.nodes.TextNode;
 import java.util.List;
 
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
 
 import org.jsoup.select.Elements;
 
+import org.jsoup.nodes.Node;
+
 public class WikiPhilosophy {
-	
+	private String web;
+	private List<String> visited;
 	final static WikiFetcher wf = new WikiFetcher();
+	final static String end = "https://en.wikipedia.org/wiki/Philosophy";
+	
+	
 	
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
@@ -27,25 +32,84 @@ public class WikiPhilosophy {
 	 * @param args
 	 * @throws IOException
 	 */
+	
 	public static void main(String[] args) throws IOException {
 		
         // some example code to get you started
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
-
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+//		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+//		Elements paragraphs = wf.fetchWikipedia(url);
+//
+//		Element firstPara = paragraphs.get(0);
+//		
+//		Iterable<Node> iter = new WikiNodeIterable(firstPara);
+//		for (Node node: iter) {
+//			if (node instanceof TextNode) {
+//				System.out.print(node);
+//			}
+//        }
 
         // the following throws an exception so the test fails
         // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+//        String msg = "Complete this lab by adding your code and removing this statement.";
+//        throw new UnsupportedOperationException(msg);
+        String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		WikiPhilosophy bop = new WikiPhilosophy(url); 
+		bop.access(); 
 	}
+	public WikiPhilosophy(String website){
+		visited = new ArrayList<String>();
+		this.web = website;
+		
+	}
+	private void access()throws IOException {
+		do {
+		 visited.add(web);
+         System.out.println ("visiting: " + web);
+         Elements paragraphs = wf.fetchWikipedia(web);
+ 		 Element firstPara = paragraphs.get(0);
+ 		 
+		 Iterable<Node> iter = new WikiNodeIterable(firstPara);
+         web = firstValidLink(iter);
+		} while (visited.contains(web) && !end.equals(web) && web.length() > 0);
+		
+//		{
+//		    visited.add(web);
+//            System.out.println ("visiting" + web);
+//            Elements paragraphs = wf.fetchWikipedia(web);
+//    		Element firstPara = paragraphs.get(0);
+//    		 
+//   		 	Iterable<Node> iter = WikiNodeIterable(firstPara);
+//			
+//            web = firstValidLink(iter);
+//            
+//		}
+		if (end.equals(web)){
+			System.out.println ("visiting: " + end);
+			System.out.println ("success");
+		} else {
+			System.out.println ("failure");
+		}
+	}
+	private String firstValidLink (Iterable<Node> iter) {
+		String nothing = "";
+	    for (Node bop: iter) {
+	         if (bop.hasAttr("href")) {
+	              for (Node temp : bop.childNodes()) {
+		              if (temp instanceof TextNode) {
+		                   String title = ((TextNode) temp).text();
+		                   if (isValid((TextNode) temp)) {
+			                   return bop.absUrl("href");
+		                    }
+		               }
+	               }
+	          }
+	     }
+	     return nothing;
+	} 
+	private boolean isValid (TextNode bop) {
+		String name= bop.text();
+		return Character.isLowerCase(name.charAt(0));
+	}
+	
 }
